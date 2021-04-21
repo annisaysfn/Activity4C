9fba0e62440cd90ff88b694b664a40bdc32af918
annisaysfn <annisa.yasfa25@gmail.com>
2021-04-21 15:15:10 +0800

    Activity4C

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.sampleconstraintlayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -99,5 +102,24 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Method untuk menampilkan menu
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Membuat kondisi jika yang dipilih adalah id mnDaftar
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            //Method untuk memanggil activity "ActivityDaftar"
+            Intent i = new Intent(getApplicationContext(), ActivityDaftar.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
